a6a87ed26f6ee6bdbf7c0b13238a960995a50f3d
PrateekKumar <145831739+Prateek-Kumar98217@users.noreply.github.com>
2025-03-09 20:30:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,23 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import NotFound from './pages/NotFound'
 import ProtectedRoutes from './components/ProtectedRoutes'
+
+function logout(){
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+function registerLogout(){
+  localStorage.clear();
+  return <Register/>
+}
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
